c6d7095fcfef06d5046d71fa61de17a67c4ee7a2
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2024-05-26 01:18:16 +0200

    Upgrading the general!

--- shared/src/commonMain/kotlin/com/github/jdw/seaofshadows/utils/General.kt
+++ shared/src/commonMain/kotlin/com/github/jdw/seaofshadows/utils/General.kt
@@ -1,13 +1,34 @@
 package com.github.jdw.seaofshadows.utils
 
+internal class GeneralException(message: String = ""): Exception("\uD83E\uDEE1$message")
+
+
 /**
  * Throws a generally exceptional objection!
  *
  * @since Sea of Shadows 0.0.3-SNAPSHOT
  * @author Johannes Alexis Wirde (johannes.wirde@gmail.com)
  */
-fun throws(): Nothing = throw Exception("\uD83E\uDEE1")
+fun throws(): Nothing = throw GeneralException()
 
+
+/**
+ * Throws a generally exceptional objection!
+ *
+ * @param message The message to rally them up with!
+ * @since Sea of Shadows 0.0.3-SNAPSHOT
+ * @author Johannes Alexis Wirde (johannes.wirde@gmail.com)
+ */
+fun throws(message: String): Nothing = throw GeneralException(message)
+
+
+/**
+ * Throws a generally exceptional objection!
+ *
+ * @param faults A collection of variables, their names and their values
+ * @since Sea of Shadows 0.0.3-SNAPSHOT
+ * @author Johannes Alexis Wirde (johannes.wirde@gmail.com)
+ */
 fun throws(vararg faults: Pair<String, Any>): Nothing {
     var message = ""
     faults.forEach { fault ->
@@ -17,6 +38,6 @@ fun throws(vararg faults: Pair<String, Any>): Nothing {
         message += "$name = $value, "
     }
 
-    throw Exception("\uD83E\uDEE1 ${message.removeSuffix(", ")}")
+    throw GeneralException(message.removeSuffix(", "))
 }
 
